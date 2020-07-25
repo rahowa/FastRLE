18de3e670a5a87ffa1a47f6784f4a5f8fba1226d
rahowa <roninfromsvf@gmail.com>
2020-07-25 17:46:12 +0300

    add main lib source file

---
+++ fast_rle.cpp
@@ -0,0 +1,102 @@
+//
+// Created by Валентин Евсеенко on 6/7/20.
+//
+
+#include "python_stuff/rle_file_wrapper.h"
+#include "python_stuff/python_utils.h"
+#include "python_stuff/converters.h"
+#include "python_stuff/about.h"
+
+#include "fast_rle/rle_file.h"
+#include "fast_rle/csv_utils.h"
+#include "fast_rle/shared_utils.h"
+#include "fast_rle/decode_utils.h"
+#include "fast_rle/encode_utils.h"
+
+#include <boost/python.hpp>
+#include <boost/python/stl_iterator.hpp>
+#include <boost/python/numpy.hpp>
+
+#include <algorithm>
+#include <numeric>
+
+
+
+
+auto readCSVWrapper(const boost::python::str & filename) -> boost::python::list {  //std::vector<RleFileWrapper>
+    auto tmpRes = readCSV(extractString(filename));
+    auto result = std::accumulate(std::make_move_iterator(tmpRes.begin()),
+            std::make_move_iterator(tmpRes.end()),
+            std::vector<RleFileWrapper>{},
+            [](auto res, auto&& rleFile){
+        res.emplace_back(RleFileWrapper(rleFile));
+        return std::move(res);
+    });
+    return stdToPythonList(result);
+}
+
+
+auto decodeRleWrapper(const boost::python::list & rlesWrapped) -> boost::python::list {
+    auto extractOriginalFn = [](const RleFileWrapper& wrapper){return wrapper.originalFile;};
+    auto extractedRles = pythonListToStd<RleFileWrapper>(rlesWrapped);
+    RleFiles result(extractedRles.size());
+    std::transform(std::make_move_iterator(extractedRles.begin()),
+                   std::make_move_iterator(extractedRles.end()),
+                   result.begin(),
+                   extractOriginalFn);
+    std::vector<cv::Mat> decodedRle(result.size());
+
+    if(result.size() > 100)
+        decodedRle = decodeRleMt(std::move(result));
+    else decodedRle = decodeRle(std::move(result));
+
+    return stdToPythonList(cvMatToNumpy(decodedRle));
+}
+
+
+auto encodeRleWrapper(const boost::python::list& imagesList) -> boost::python::list {
+    auto converterFn = [&](boost::python::numpy::ndarray&& img){return ndarrayToCv(std::move(img));};
+    auto images = pythonListToStd<boost::python::numpy::ndarray, cv::Mat>(imagesList, converterFn);
+    std::cout << "Images size 0:  " << images.size() << std::endl;
+
+    auto imgSizes = std::accumulate(images.cbegin(), images.cend(),
+            std::vector<cv::MatSize>{},
+             [](auto res, auto & img){
+        res.emplace_back(img.size);
+        return std::move(res);
+    });
+
+    auto imagesCopy = images;
+    std::vector<RleFileWrapper> result(imagesCopy.size());
+    std::vector<std::string > encodedRles(imagesCopy.size());
+
+    if(imagesCopy.size() < 100)
+        encodedRles = encodeRle(std::move(imagesCopy));
+    else encodedRles = encodeRleMt(std::move(imagesCopy));
+
+    for(size_t idx = 0; idx < result.size(); ++idx){
+        result.at(idx) = {boost::python::str(std::string("image_" + std::to_string(idx) + ".png")),
+                                boost::python::str(encodedRles.at(idx).c_str()),
+                            boost::python::make_tuple(imgSizes.at(idx)[0], imgSizes.at(idx)[1])};
+    }
+    return stdToPythonList(result);
+}
+
+
+BOOST_PYTHON_MODULE(fast_rle)
+{
+    using namespace boost::python;
+    boost::python::numpy::initialize();
+
+    def("about", &about);
+
+    class_<RleFileWrapper>("RleFile", init<boost::python::str, boost::python::str, boost::python::tuple>())
+            .def("size", &RleFileWrapper::imageSize)
+            .def("filename", &RleFileWrapper::filename)
+            .def("rle", &RleFileWrapper::rle)
+            ;
+
+    def("read_csv", &readCSVWrapper, args("filename"));
+    def("decode_rle", &decodeRleWrapper, args("rles"));
+    def("encode_rle", &encodeRleWrapper, args("images"));
+}
