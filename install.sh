fe3b23d2c91864e888ce52dc328f7bd4a141f396
rahowa <roninfromsvf@gmail.com>
2020-10-15 17:27:46 +0300

    fix installation pipeline

--- install.sh
+++ install.sh
@@ -2,14 +2,20 @@ pip install numpy
 pip install conan
 
 mkdir build 
-python_path=$(wich python)
-
-conan install . -s build_type=Release --install-folder=build --build=boost -o boost:python_executable=$python_path  -o boost:with_python=True --build=opencv --build=protobuf
+python_path=$(which python)
 
 cd build 
+conan install .. -s compiler.version=12.0 -s build_type=Release \
+ --install-folder=. \
+ -o boost:python_executable=$python_path \
+ -o boost:without_python=False \
+ --build=boost \
+ --build=opencv \
+ --build=protobuf \
+ --build=missed
 
-cmake -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR=$python_path ..
-make -j $(nproc)
-make install   
+# cmake -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR=$python_path ..
+# cmake --build . && cmake --install . 
 
+cd ..
 python setup.py install --user
