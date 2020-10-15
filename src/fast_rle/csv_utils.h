9009b99d7cae2fa2394ff41799189da6d879c65d
rahowa <roninfromsvf@gmail.com>
2020-10-15 17:38:41 +0300

    add saving with temp files

--- src/fast_rle/csv_utils.h
+++ src/fast_rle/csv_utils.h
@@ -6,9 +6,10 @@
 #define FAST_RLE_CSV_UTILS_H
 
 #include "rle_file.h"
+#include "to_parts.h"
+#include "csv_writer.hpp"
 #include "shared_utils.h"
 #include "fast-cpp-csv-parse/csv.h"
-#include "to_parts.h"
 
 #include <fstream>
 #include <thread>
@@ -24,5 +25,8 @@ auto readCSV(std::string&& filename) -> RleFiles;
 auto saveCSV(std::string&& filename, RleFiles&& encodings) -> void;
 
 auto saveCSVMt(std::string&& filename, RleFiles&& encodings) -> void;
+
+auto saveCSVWithTmpFiles(std::string && filename, RleFiles&& encodings) -> void;
+
 
 #endif //FAST_RLE_CSV_UTILS_H
