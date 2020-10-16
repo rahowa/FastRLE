759906cb536d222a39e0483236d8b5d317fe81fd
rahowa <roninfromsvf@gmail.com>
2020-10-16 14:53:14 +0300

    update installation pipeline

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-pip install numpy 
-pip install conan
-
+git submodule update --init --recursive
 mkdir build 
-python_path=$(which python)
-
 cd build 
-conan install .. -s compiler.version=12.0 -s build_type=Release \
+
+python -m venv env
+source ./env/bin/activate 
+pip install numpy conan pandas
+
+python_path=$(which python)
+ 
+conan install .. -s build_type=Release \
  --install-folder=. \
  -o boost:python_executable=$python_path \
  -o boost:without_python=False \
  --build=boost \
  --build=opencv \
  --build=protobuf \
- --build=missed
-
-# cmake -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR=$python_path ..
-# cmake --build . && cmake --install . 
+ --build=missing
 
 cd ..
 python setup.py install --user
