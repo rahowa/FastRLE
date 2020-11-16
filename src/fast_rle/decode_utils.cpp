e8eefd41ec2c5cf98c4dd882e6079114f1081fe4
rahowa <roninfromsvf@gmail.com>
2020-11-16 12:11:35 +0300

    refactor mt version with parallel transform

--- src/fast_rle/decode_utils.cpp
+++ src/fast_rle/decode_utils.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "decode_utils.h"
-
+#include <execution>
 
 
 auto decodeRle(RleFiles&& rles) -> std::vector<cv::Mat> {
@@ -11,53 +11,38 @@ auto decodeRle(RleFiles&& rles) -> std::vector<cv::Mat> {
     std::transform(std::make_move_iterator(rles.begin()),
                    std::make_move_iterator(rles.end()),
                    result.begin(),
-            [](auto && rle){return rle2mask(std::move(rle));});
+                   [](auto && rle){return decodeRle(std::move(rle));});
     return result;
 }
 
 
 auto decodeRleMt(RleFiles&& rles) -> std::vector<cv::Mat> {
-    std::vector<cv::Mat> masks;
-
-    auto numParts = std::max(2u, std::thread::hardware_concurrency());
-    auto parts = ToParts(numParts, rles.begin(), rles.end());
-    
-    std::vector<std::future<std::vector<cv::Mat>>> futures;
-    while (!parts.done()){
-        auto part = parts.get();
-        futures.push_back(std::async(std::launch::async,
-                           [=]{return rle2mask(part.first, part.second);}));
-    }
-
-    std::for_each(std::make_move_iterator(futures.begin()),
-                  std::make_move_iterator(futures.end()), 
-                  [&](std::future<std::vector<cv::Mat>> && fut){
-                      auto tmpVector = fut.get();
-                      masks.insert(masks.end(), 
-                                   std::make_move_iterator(tmpVector.begin()),
-                                   std::make_move_iterator(tmpVector.end()));
-                  });
+    std::vector<cv::Mat> masks(rles.size());
+    std::transform(std::execution::par_unseq, 
+                   rles.begin(),
+                   rles.end(),
+                   masks.begin(),
+                   [](auto && rle){return decodeRle(std::move(rle));});
     return masks;
 }
 
 
-auto rle2mask(const RleFile& rleFile) -> cv::Mat {
+auto decodeRle(const RleFile& rleFile) -> cv::Mat {
     auto rle = parseRle(std::move(rleFile.rle));
     cv::Mat mask(1, rleFile.imageSize.height * rleFile.imageSize.width, CV_8UC1);
-    size_t idx = 0;
+
     auto maskBegin = rle.begin();
     auto maskEnd = rle.end();
-    cv::Mat res(rleFile.imageSize, CV_8UC1);
-    while (maskBegin != maskEnd){
+    for(size_t idx = 0; maskBegin != maskEnd; std::advance(maskBegin, 2), idx +=2){
         size_t start = std::max(0, std::stoi(rle.at(idx)) - 1);
         size_t lenght = std::max(0, std::stoi(rle.at(idx + 1)));
         size_t finish = std::max(0ul, start + lenght);
-        for(size_t row = start; row < finish; ++row)
-            mask.at<uchar>(row) = 255;
 
-        std::advance(maskBegin, 2);
-        idx += 2;
+        auto it1 = mask.begin<uchar>();
+        auto it2 = mask.begin<uchar>();
+        std::advance(it1, start);
+        std::advance(it2, finish);
+        std::fill(it1, it2, 255);
     }
-    mask = mask.reshape(1, rleFile.imageSize.height);
-    return mask;
+    return mask.reshape(1, rleFile.imageSize.height);;
 }
